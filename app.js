496c7500be6cff3dfa839461cad0f5598685e928
Mohammad Aslam <Taiyab.aslam728@gmail.com>
2023-01-06 17:15:07 -0600

    fixed quoatations problems

--- app.js
+++ app.js
@@ -28,26 +28,26 @@ resetBtn.addEventListener('click', () => {
 
 
 const answers = [
-    "Yes, definitely",
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "You may rely on it",
-    "As I see it, yes",
-    "Most Likely",
-    "Outlook good",
-    "Signs point to yes",
-    "Reply hazy, try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",
-    "Don’t count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful",
-  ];
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
